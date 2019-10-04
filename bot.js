b8d586cd72fcd9fdfbea369ff9e9f87ecf5a0d5e
will.robson <wrobson2244@gmail.com>
2019-10-04 15:21:25 +0100

    Example guild and member code

--- bot.js
+++ bot.js
@@ -2,14 +2,34 @@ var Discord = require('discord.js');
 var auth = require('./auth.json');
 var schedule = require('node-schedule');
 
-var bot = new Discord.Client();
-bot.login(auth.token);
+var client = new Discord.Client();
+client.login(auth.token);
 
-bot.on('ready', function (evt) {
+client.on('ready', function (evt) {
     console.log("Bot is ready");
-    bot.user.setActivity("Beep Boop. I am a bot.");
-    setCronJob();
+    client.user.setActivity("Beep Boop. I am a bot.");
+    //setCronJob();
+    let roles = client.guilds.first().roles
+
+    for (var [key, value] of roles) {
+        console.log(value.name);
+      }
+
+    let users = client.guilds.first().members
+
+    for (var [key, value] of users)
+    {
+        console.log(value.roles)
+    }
+
 });
+
+client.on('message', message => {
+    
+    var member = message.member;
+    console.log(member.nickname)
+    }
+  );
 
 function setCronJob()
 {
